48cba49343e897dcf4bfb857119451becdf0f9f8
matthsworld <matthsworld@gmail.com>
2022-03-16 20:06:39 +0800

    My WP fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,10 @@
 /**
  * _islower - shows 1 if the input is a
  * lowercase character, another cases shows 0
- * 
  * @c: The character in ASCII code
- *
  * Return: 1 for lowercase, 0 for the rest
  */
+
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
